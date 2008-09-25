adb98b9c8152b3a7f5180deca034ed0e6c2b98b9
sivel <sivel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-25 00:36:55 +0000

    Checking and tagging v1.0 of easy-disable-visual-editor
    git-svn-id: https://plugins.svn.wordpress.org/easy-disable-visual-editor/tags/1.0@66181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-disable-visual-editor.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Easy Disable Visual Editor
+Plugin URI: http://sivel.net/wordpress/
+Description: Disables the Visual editor globally.
+Author: Matt Martz <mdmartz@sivel.net>
+Author URI: http://sivel.net/
+Version: 1.0
+
+        Copyright (c) 2008 Matt Martz (http://sivel.net)
+        Page Restrict is released under the GNU Lesser General Public License (LGPL)
+        http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+if ( is_admin () )
+	add_filter ( 'user_can_richedit' , create_function ( '$a' , 'return false;' ) , 50 );
+?>
